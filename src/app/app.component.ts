a4c421313d84d0e2cb0d514f5a039a148551d1df
kqmirasol <kqmirasol@icloud.com>
2025-01-20 12:47:01 +0800

    initital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeService } from './employee.service';
+import { CommonModule } from '@angular/common';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-services-demo';
+   public employees: {
+     id: number;
+     firstname: string;
+     lastname: string;
+     email: string;
+   }[] = [];
+   products: any[] = [];
+ 
+
+   constructor(private _employeeService: EmployeeService, private productsService: ProductsService) {}
+
+   ngOnInit() {
+     this.employees = this._employeeService.getEmployees();
+      this.products = this.productsService.getproducts();
+   }
+
+
 }
---
+++ src/app/products.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  getproducts(){
+    return[{
+      id: "P-101",
+      productname: 'Logitech Mouse',
+      description: 'Dizon',
+      prices: 899
+    },
+    {
+      id: "P-102",
+      productname: 'JBL BT Speaker',
+      description: 'Waterproof Radio 360 Surround',
+      prices: 1099
+    },
+    {
+      id: "P-103",
+      productname: 'Mechanical Keyboard',
+      description: 'Hot-swappable RGB Backlit',
+      prices: 2395
+    },
+    {
+      id: "P-104",
+      productname: 'Oculus Meta',
+      description: 'All-in-one Gaming Headset',
+      prices: 22450
+    },
+    {
+      id: "P-105",
+      productname: 'Karlo Mirasol',
+      description: 'Pogi',
+      prices: 999999999
+    }]
+  }
+}
